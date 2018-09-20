7e17516b21c20012fbbe78cf0c9463a198fc7198
Mariano Esturrica Mayo <mariano_esturrica@hotmail.com>
2018-09-20 09:58:13 +0200

    Add files via upload

---
+++ HelloWorldMavenDockerized/src/main/java/com/privalia/business/ICalculator.java
@@ -0,0 +1,12 @@
+package com.privalia.business;
+
+public interface ICalculator {
+	/* All interface methods are public by default */
+	int sum(int num1, int num2);
+
+	int substract(int num1, int num2);
+
+	int multiply(int num1, int num2);
+
+	int divide(int num1, int num2);
+}
